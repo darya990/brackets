4795f706d58bc5275d6c55f7c04ec31f64e69549
darya990 <dasha.shibkova@mail.ru>
2019-03-20 20:59:00 +0300

    feat: brackets is checked

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ let tempStr = str;
+ for (let strPart=tempStr.length; strPart>=0; strPart--){
+   for(let bracketPart=0; bracketPart<bracketsConfig.length; bracketPart++){
+if(tempStr[strPart]===bracketsConfig[bracketPart][0]){
+  tempStr=tempStr.replace(bracketsConfig[bracketPart].join(''), '')
+}
+   }
+ }
+ return tempStr === '';
 }
